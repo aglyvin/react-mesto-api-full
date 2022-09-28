d19afe67dd069a67fe3ff670f306ca75db538d22
aglyvin <alexej@glyvin.com>
2022-09-28 20:12:13 +0300

    a

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000 } = process.env;
 
 const app = express();
-app.use(cors({ Origin: 'https://aglyvin.nomoredomains.sbs' }));
+app.use(cors());
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
