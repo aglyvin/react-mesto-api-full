6c9874ed90bc2039c53da719fa78dae9178f1ca0
aglyvin <alexej@glyvin.com>
2022-09-28 18:33:50 +0300

    add http

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -5,6 +5,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
     next(new UnauthorizeError('Необходима авторизация'));
+    return;
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
